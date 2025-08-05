f955845431bc3b606e06b899186063e75dcb6b50
Supriya <150614456+Supriya-Battu@users.noreply.github.com>
2025-08-05 09:42:32 +0530

    Update LinkedList.java

--- Java projects/CollectionFrameWorkApp/src/com/collection/LinkedList.java
+++ Java projects/CollectionFrameWorkApp/src/com/collection/LinkedList.java
@@ -1,10 +1,31 @@
 package com.collection;
 
-public class LinkedList {
+import java.util.LinkedList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class LList {
 
+	public static void main(String[] args)
+	{
+		LinkedList al = new LinkedList();
+		//1.insert
+				al.add(10);
+				al.add("priya");
+				al.add(true);
+				al.add(123456789);
+				//adding duplicate
+				al.add(10);
+				//adding null values
+				al.add(null);
+				System.out.println(al);
+				System.out.println(al.size());
+				//2.delete/remove
+				al.remove(2);//2 is index
+				System.out.println(al);
+				System.out.println(al.size());
+				//3.Search
+				System.out.println(al.contains("priya"));
+				//4.Get/retrieve
+				System.out.println(al.get(2));//2 is index 
 	}
-
 }
+
